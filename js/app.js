08acb6b5e69c3ffdba59bef43b464d62459dd18b
fanshao <fanshaobo_007@163.com>
2018-09-12 20:33:29 +0800

    Site updated: 2018-09-12 20:33:28

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ window.onload = function() {
             var top = $toc.offsetTop;
             var left = $toc.offsetLeft;
             var width = $toc.offsetWidth;
-            if(getScrollTop() + 100 <= top){
+            if(getScrollTop() - 100 <= top){
                 $toc.style = "";
             } else {
                 $toc.style.position = "fixed";
